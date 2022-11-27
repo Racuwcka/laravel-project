34b41063cebe4625a49a1b0de870acf37f0a30b3
Racuwcka <13zhora13@mail.ru>
2022-11-27 20:10:22 +0400

    creating jwt Token

--- app/Services/Post/Service.php
+++ app/Services/Post/Service.php
@@ -12,6 +12,8 @@ class Service
         unset($data['tags']);
         $post = Post::create($data);
         $post->tags()->attach($tags);
+
+        return $post;
     }
 
     public function update($post, $data)
@@ -20,6 +22,8 @@ class Service
         unset($data['tags']);
         $post->update($data);
         $post->tags()->sync($tags);
+
+        return $post->fresh();
     }
 
 }
